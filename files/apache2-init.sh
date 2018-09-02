e7dd44b991e93f07deeca9bf3e935cf034aba5f1
Mariano Barcia <mariano.barcia@worldfirst.com>
2018-09-02 20:31:08 +0100

    DRUPSIBLE-237 exec the process

--- files/apache2-init.sh
+++ files/apache2-init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-source /etc/apache2/envvars && /usr/sbin/apache2 -f /etc/apache2/apache2.conf -DNO_DETACH
+source /etc/apache2/envvars && exec /usr/sbin/apache2 -f /etc/apache2/apache2.conf -DNO_DETACH
 
